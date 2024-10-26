d4ee5760fc552b7fe4400e2bc29a6bd38da17086
George Tian <me@georgetian.com>
2024-10-26 19:34:39 +1100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,38 @@
+set -e
+
 docker_not_installed() {
-  # Check for older version (`docker-compose`) or newer plugin-based (`docker compose`) version
-  if docker compose version >/dev/null 2>&1; then
-    return 0 # installed
-  else
-    return 1 # not installed
-  fi
+    # Check for older version (`docker-compose`) or newer plugin-based (`docker compose`) version
+    if docker compose version >/dev/null 2>&1; then
+        return 0 # installed
+    else
+        return 1 # not installed
+    fi
 }
 
-# install_docker_compose() {
-#     # https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
-#     # Add Docker's official GPG key:
-#     sudo apt-get update
-#     sudo apt-get install ca-certificates curl
-#     sudo install -m 0755 -d /etc/apt/keyrings
-#     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-#     sudo chmod a+r /etc/apt/keyrings/docker.asc
+install_docker_compose() {
+    # https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-#     # Add the repository to Apt sources:
-#     echo \
-#     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#     sudo apt-get update
-#     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# }
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
 
 if docker_not_installed; then
     install_docker_compose
 fi
 
+git pull
+git submodule update --init --recursive
+git submodule update --recursive --remote
 
-# git 
+sudo docker compose up --build -d
