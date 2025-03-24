5393ed16c5bb5b7e87203c121c84391c95cead16
George Tian <me@georgetian.com>
2025-03-25 00:03:11 +1100

    update

--- deploy.sh
+++ deploy.sh
@@ -26,14 +26,29 @@ install_docker_compose() {
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
-if docker_not_installed; then
-    echo "Docker compose not installed, installing..."
-    install_docker_compose
-fi
+# if docker_not_installed; then
+#     echo "Docker compose not installed, installing..."
+#     install_docker_compose
+# fi
 
-git pull
-git submodule update --init --recursive
-git submodule update --recursive --remote
+
+env_branches=("main" "dev" "sandpit")
+env_names=("prod" "dev" "sandpit")
+components=("backend" "frontend")
+
+# clones or pulls each env for back/frontend
+for component in "${components[@]}"; do
+    for ((env=0; env<${#env_names[@]}; env++)); do
+        dir="${component}-${env_names[env]}"
+        if [ ! -d "$dir" ]; then
+            echo "$dir doesn't exist, cloning"
+            git clone -b ${env_branches[env]} --single-branch "https://github.com/georgetian3/llmanipulate-${component}.git" "${component}-${env_names[env]}"
+        else
+            echo "$dir exists, updating"
+            git -C $dir pull
+        fi
+    done
+done
 
 sudo docker compose up --build -d
-sudo docker restart llmanipulate-server
+sudo docker restart server
